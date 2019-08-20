db50e49358ae90bcfdb32ee284e69b87b1ef6971
Ceceee <jil717@ucsd.edu>
2019-08-19 17:48:15 -0700

    [SWDEV-345]Update login test

--- test/login.test.js
+++ test/login.test.js
@@ -2,23 +2,32 @@ require('./common')
 
 describe('DCCN User Manager', () => {
     //before(authenticateWithTestAcct)
-    try{
-        for (var i = 0; i < 1000; i++){
-            context('login',() => {
-                it('should send a login request', async () => {
+    flag = 0
+    break_flag = 0
+    
+    context('login',() => {
+        it('should send a login request', async () => {
+            try{
+                for (var i = 0; i < 10; i++){
                     const loginfo = await reqA('POST', '/login',{
-                        email: 'liyifan9308@gmail.com',
-                        password: 'ankr1234'
-                    })
-                    log.info('login', JSON.stringify(loginfo, null, ' '))
-                    console.log(loginfo.user.attributes.value)
-                    expect(loginfo.authentication_result.access_token.length).to.be.at.least(0)
-                })
-            })
-
+                    email: 'ankrtestuser1@mailinator.com',
+                    password: 'ankr1234'
+                })        
+                log.info('login', JSON.stringify(loginfo, null, ' '))
+                console.log(loginfo.user.attributes.value)
+                expect(loginfo.authentication_result.access_token.length).to.be.at.least(0)
+                if(break_flag = 1){
+                    break
+                }
+            }
+        }catch(e){
+            flag = 1
+            break_flag = 1
         }
-    }catch(e){
-        throw e
-    }
-    throw new Error('error for logging in too many times')  
+        if(flag = 0){
+            throw new Error('error for logging in too many times')  
+        }
+    })
+})
+    
 })
